229e5fa1002b5c30330c5a2430a8aa8d356a5369
Ian Trancynger <itrancynger@gmail.com>
2022-09-13 03:08:01 -0400

    Changes to be committed: 	modified:   build_database/allGamesLoop.py

--- build_database/allGamesLoop.py
+++ build_database/allGamesLoop.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import pandas
+import time
 
 startUrl = 'https://statsapi.web.nhl.com/api/v1/game/'
 endUrl = '/feed/live'
@@ -11,18 +12,27 @@ url = startUrl + str(startYear) + '0' + str(startGame) + endUrl
 y = 0
 gameUrlList = []
 
+n = 0
+runStartTime = time.time()
+
 while startYear < 2022:
     response = requests.get(url)
     gameJson = response.json()
     data = json.dumps(gameJson)
     gameData = json.loads(data)
 
-    if 'gamePk' in gameData:
-        print(gameData['gamePk'])
-        gameUrlList.append(url)
+    time.sleep(0.25)
 
+
+    if 'gamePk' in gameData:
+        gameUrlList.append(url)
         startGame += 1
         url = startUrl + str(startYear) + '0' + str(startGame) + endUrl
+        
+        n += 1 
+        currentTime = time.time()
+        elapsed = "%.2f" % (currentTime - runStartTime)
+        print(str(n) + '   ' + str(gameData['gamePk']) + '   ' + elapsed)
 
     else: 
         startGame = 20001
